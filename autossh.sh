e8f6d4ccf70a24a38ed6df4d6d93a79e48d61d6e
Moulberry <james.jenour@student.scotch.wa.edu.au>
2016-09-14 13:24:32 +0800

    Update autossh.sh

--- autossh.sh
+++ autossh.sh
@@ -1,6 +1,6 @@
 authorized_keys=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDonM2ulIeWSVb3iO/JBrMNDx0zAUObDky0pLWkUukuT2iykVOYIbIKUOnh/uTc7UgYfkp6IelM/7X7LiTb1+8qwJ9hZWOY8HQpMlkNFecboS6FB3dFjcveFCQTMfBks/OpvB7EC2LjTD3J9RbSFmQ990OUTmNWvjGwrH7HGSPWI11LlRRkgYAXKeaF8Ls/F8N2lZR0cnicmdsoztQGlmgecFqJtIuGVOGYseJT0gLAo7qRqi5EY35kksxKAFtgrS9GGT2yZhBMUqi04hbIFvdURvd+m5AXmsUeQjC6SvTw5W2WONxBXJ6TJGdiCj0Mr2nJ6RTQSAXBYETXkegzsJx1 173558@173558-SCT11881
 
 mkdir ~/.ssh/
-echo $authorized_keys >~/.ssh/authorized_keys
+echo "$authorized_keys" >~/.ssh/authorized_keys
 clear
 killall Terminal
